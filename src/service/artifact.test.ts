ac4ea3658aab7da87fb053f10d778e7c9b0e330d
Brett Wright <wright.brett@gmail.com>
2021-12-20 12:26:34 +0100

    created test

---
+++ src/service/artifact.test.ts
@@ -0,0 +1,45 @@
+import request from "supertest";
+import { createRouter } from "./router";
+import express from "express";
+import { ConfigReader } from "@backstage/config";
+import { getVoidLogger } from "@backstage/backend-common";
+
+describe("createRouter", () => {
+  let app: express.Express;
+
+  beforeAll(async () => {
+    const router = await createRouter({
+      logger: getVoidLogger(),
+      config: new ConfigReader({}),
+    });
+    app = express().use(router);
+  });
+
+  describe("GET /health", () => {
+    it("returns ok", async () => {
+      const response = await request(app).get("/health");
+
+      expect(response.statusCode).toEqual(200);
+      expect(response.body).toEqual({ status: "ok" });
+    });
+  });
+
+  describe("GET /artifacts", () => {
+    it("return repository info", async () => {
+      const response = await request(app)
+        .get("/artifacts")
+        .query({ project: "es", repository: "pipectl" });
+
+      expect(response.statusCode).toEqual(200);
+      expect(Array.isArray(response.body)).toBeTruthy();
+
+      expect(response.body[0]).toHaveProperty("size");
+      expect(response.body[0]).toHaveProperty("tag");
+      expect(response.body[0]).toHaveProperty("pullTime");
+      expect(response.body[0]).toHaveProperty("pushTime");
+      expect(response.body[0]).toHaveProperty("projectID");
+      expect(response.body[0]).toHaveProperty("repoUrl");
+      expect(response.body[0]).toHaveProperty("vulnerabilities");
+    });
+  });
+});
