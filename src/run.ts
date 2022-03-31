ce5735c4e2113e76198976821e5a966c5c1533ac
Brett Wright <brett.wright@bestseller.com>
2022-03-31 14:23:59 +0200

    removed things

--- src/run.ts
+++ src/run.ts
@@ -15,16 +15,14 @@
  */
 
 import { getRootLogger } from "@backstage/backend-common";
-import { ConfigReader } from "@backstage/config";
 import yn from "yn";
 import { startStandaloneServer } from "./service/standaloneServer";
 
 const port = process.env.PLUGIN_PORT ? Number(process.env.PLUGIN_PORT) : 7007;
 const enableCors = yn(process.env.PLUGIN_CORS, { default: false });
 const logger = getRootLogger();
-const config = new ConfigReader({});
 
-startStandaloneServer({ port, enableCors, logger, config }).catch((err) => {
+startStandaloneServer({ port, enableCors, logger }).catch((err) => {
   logger.error(err);
   process.exit(1);
 });
