de1fbd3771cf69da6caf8d9d48f00e93ed8b36f5
Eren <32169493+locomoco28@users.noreply.github.com>
2023-09-15 15:30:23 +0200

    use scan overview parameter to get vulns with single request

--- src/service/artifact.ts
+++ src/service/artifact.ts
@@ -14,7 +14,7 @@ export async function getArtifacts(
     repo = repository.replace('/', '%252F')
   }
 
-  const url = `${baseUrl}/api/v2.0/projects/${project}/repositories/${repo}/artifacts?page=1&page_size=10&with_tag=true&with_label=false&with_scan_overview=false&with_signature=false&with_immutable_status=false`
+  const url = `${baseUrl}/api/v2.0/projects/${project}/repositories/${repo}/artifacts?page=1&page_size=10&with_tag=true&with_label=false&with_scan_overview=true&with_signature=false&with_immutable_status=false`
 
   const response = await fetch(url, {
     headers: {
@@ -33,69 +33,14 @@ export async function getArtifacts(
         push_time: string
         project_id: number
       }) => {
-        const vulnUrl: string = `${baseUrl}${element.addition_links.vulnerabilities.href}`
 
-        const vulns: Root = await fetch(vulnUrl, {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Basic ${Base64.encode(`${username}:${password}`)}`,
-          },
-        }).then((res: { json: () => any }) => res.json())
-
-        const vulnKey =
-          'application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0'
-
-        if (vulns[vulnKey] === undefined) {
-          vulns[vulnKey] = {
-            generated_at: '',
-            scanner: {
-              name: 'undefined',
-              vendor: 'undefined',
-              version: 'undefined',
-            },
-            severity: 'Unknown',
-            vulnerabilities: [],
-          }
-        }
-
-        let severity = vulns[vulnKey].severity
-        if (severity === 'Unknown') {
-          severity = 'None'
-        }
-
-        const projectId = element.project_id
-        let critical = 0
-        let high = 0
-        let medium = 0
-        let low = 0
-        let none = 0
-
-        vulns[vulnKey].vulnerabilities.map((value) => {
-          switch (value.severity) {
-            case 'Low': {
-              low += 1
-              break
-            }
-            case 'Medium': {
-              medium += 1
-              break
-            }
-            case 'High': {
-              high += 1
-              break
-            }
-            case 'Critical': {
-              critical += 1
-              break
-            }
-            default:
-              none += 1
-          }
-        })
-
+        
         const generatedTag = element.tags?.length
-          ? element.tags[0].name
-          : 'undefined'
+        ? element.tags[0].name
+        : 'undefined'
+        
+        const scanOverview = element.scan_overview["application/vnd.security.vulnerability.report; version=1.1"]
+        const { Critical, High, Medium, Low } = scanOverview.summary.summary
 
         const art: Artifact = {
           size: Math.round((element.size / 1028 / 1028) * 100) / 100,
@@ -108,13 +53,13 @@ export async function getArtifacts(
             '%2F'
           )}`,
           vulnerabilities: {
-            count: Object.keys(vulns[vulnKey].vulnerabilities).length,
-            severity: severity,
-            critical: critical,
-            high: high,
-            medium: medium,
-            low: low,
-            none: none,
+            count: scanOverview.summary.total,
+            severity: scanOverview.severity,
+            critical: Critical,
+            high: High,
+            medium: Medium,
+            low: Low,
+            none: scanOverview.summary.total - Critical - High - Medium - Low,
           },
           id: projectId + generatedTag + element.push_time,
         }
